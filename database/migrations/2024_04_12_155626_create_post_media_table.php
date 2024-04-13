7c74366c5c0e33977efeb3a2f383857dbd7ca2e7
giankyle989 <kylelaguerta989@gmail.com>
2024-04-13 20:41:09 +0800

    feat:completed tables fields

--- database/migrations/2024_04_12_155626_create_post_media_table.php
+++ database/migrations/2024_04_12_155626_create_post_media_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('post_media', function (Blueprint $table) {
             $table->id();
+            $table->string('media_location');
             $table->timestamps();
         });
     }
