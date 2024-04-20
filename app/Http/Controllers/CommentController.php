8543bb38714057c1e2d4e3089aac00dc9219ff1f
giankyle989 <kylelaguerta989@gmail.com>
2024-04-20 17:29:56 +0800

    feat: added comment backend functions

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
+use PDO;
 
 class CommentController extends Controller
 {
@@ -11,7 +15,8 @@ class CommentController extends Controller
      */
     public function index()
     {
-        //
+        $comment = Comment::get();
+        return response($comment);
     }
 
     /**
@@ -19,15 +24,32 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $user_id = $request->user_id;
+        $post_id = $request->post_id;
 
+        $user = User::findOrFail($user_id);
+        $post = Post::findOrFail($post_id);
+    
+        $comment = Comment::create([
+            'comment_text' => $request->get('comment_text'),
+            'post_id' => $post->id,
+            'user_id' => $user->id
+        ]);
+    
+        return response($comment);
+    }
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        //
+        $comment = Comment::find($id);
+
+        if (!$comment) {
+            return response()->json('No comment with that ID');
+        }
+    
+        return response()->json($comment);
     }
 
     /**
@@ -43,6 +65,9 @@ class CommentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $comment = Comment::findOrFail($id);
+        $comment->delete();
+
+        return response()->json('Deleted');
     }
 }
